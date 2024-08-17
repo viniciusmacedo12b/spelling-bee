e0cccc0822d15e17b7eaa40e5049f6824fdfcf61
vinicius <vm839469@gmail.com>
2024-08-17 18:27:59 -0300

    segundo commit do projeto, arrumando bugs e adicionando dificuldade

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var palavra
-const jogo = ["Joshua", "Gideon", "Deborah", "Zealous", "Provider", "Sustainer", "Solomon", "Creator", "Elijah", "Mighty", "Isaiah", "Invisible", "Jacob", "Ezekiel", "Majestic", "Infinite", "Esther", "Redemption", "Sovereign", "Nehemiah", "Immortal", "Jonathan", "Jonathan", "Redeemer", "Truthful", "Nazareth", "Golgotha", "Deliverer", "Shepherd", "Messiah", "Gracious", "Shekinah", "Atonement", "Merciful", "Incorruptible", "Incorruptible", "Righteousness", "Thessalonians", "Sanctification", "Melchizedek", "Incomparable", "Justification", "Immanent", "Transcendent", "Glorification", "Regeneration", "Omnipresent", "Compassionate", "Incorrumtible", "Unfathomable"]
-palavra = jogo[Math.floor(Math.random() * 51)]
+var jogo = []
+palavra = jogo[Math.floor(Math.random() * 50)]
 console.log(palavra)
 var correto = document.getElementById("acerto")
 var erro = document.getElementById("erro")
@@ -34,7 +34,59 @@ function proximo(){
         scorenumber = 0
         scoretext.innerHTML = "score:"+scorenumber
     }
-    palavra = jogo[Math.floor(Math.random() * 51)];
+    if(diff == 1){
+        palavra = jogo[Math.floor(Math.random() * 16)]
+    }
+    else if (diff == 2)
+    {
+        palavra = jogo[Math.floor(Math.random() * 16)]
+    }
+    else if (diff == 3)
+    {
+        palavra = jogo[Math.floor(Math.random() * 15)]
+    }
+    else{
+        palavra = jogo[Math.floor(Math.random() * 50)]
+    }
 }
 var correcao = document.createElement("p")
 document.getElementById("card").appendChild(correcao)
+
+function facil(){
+    localStorage.setItem("diff", 1)
+    window.location.href = "index2.html";
+}
+function medio(){
+    localStorage.setItem("diff", 2)
+    window.location.href = "index2.html";
+}
+function dificil(){
+    localStorage.setItem("diff", 3)
+    window.location.href = "index2.html";
+}
+function aleatorio(){
+    localStorage.setItem("diff", 4)
+window.location.href = "index2.html";
+}
+var diff = localStorage.getItem("diff")
+if (diff == 1)
+{
+    jogo = ["Joshua", "Gideon", "Deborah", "Zealous", "Provider", "Sustainer", "Solomon", "Creator", "Elijah", "Mighty", "Isaiah", "Invisible", "Jacob", "Ezekiel", "Majestic", "Infinite", "Esther"]
+    palavra = jogo[Math.floor(Math.random() * 16)]
+}
+if (diff == 2)
+    {
+
+        jogo = ["Redemption", "Sovereign", "Nehemiah", "Immortal", "Jonathan", "Jonathan", "Redeemer", "Truthful", "Nazareth", "Golgotha", "Deliverer", "Shepherd", "Messiah", "Gracious", "Shekinah", "Atonement", "Merciful"]
+        palavra = jogo[Math.floor(Math.random() * 16)]
+    }
+    if (diff== 3)
+        {
+            jogo = ["Incorruptible", "Incorruptible", "Righteousness", "Thessalonians", "Sanctification", "Melchizedek", "Incomparable", "Justification", "Immanent", "Transcendent", "Glorification", "Regeneration", "Omnipresent", "Compassionate", "Unfathomable"]
+            palavra = jogo[Math.floor(Math.random() * 15)]
+        }
+        if (diff== 4)
+            {
+                jogo = ["Joshua", "Gideon", "Deborah", "Zealous", "Provider", "Sustainer", "Solomon", "Creator", "Elijah", "Mighty", "Isaiah", "Invisible", "Jacob", "Ezekiel", "Majestic", "Infinite", "Esther", "Redemption", "Sovereign", "Nehemiah", "Immortal", "Jonathan", "Jonathan", "Redeemer", "Truthful", "Nazareth", "Golgotha", "Deliverer", "Shepherd", "Messiah", "Gracious", "Shekinah", "Atonement", "Merciful", "Incorruptible", "Incorruptible", "Righteousness", "Thessalonians", "Sanctification", "Melchizedek", "Incomparable", "Justification", "Immanent", "Transcendent", "Glorification", "Regeneration", "Omnipresent", "Compassionate","Unfathomable"]
+                palavra = jogo[Math.floor(Math.random() * 50)]            
+            }
